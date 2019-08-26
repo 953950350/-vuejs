11ac7ec60346d81dee5f5b210f009e8ba4bac14f
lichengwen <953950350@qq.com>
2019-08-27 00:24:01 +0800

    feat: 第三章开始

---
+++ 代码/Array变化侦测/index.js
@@ -0,0 +1,24 @@
+const arrayProto = Array.prototype
+
+const arrayMethods = Object.create(arrayProto)
+
+;[
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'sort',
+    'reverse',
+    'splice'
+].forEach(method => {
+    // 将原始方法缓存
+    const original = arrayMethods[method];
+    Object.defineProperty(arrayMethods, method, {
+        configurable: true,
+        enumerable: false,
+        writable: true,
+        value(...arg) {
+            return original.apply(this, arg);
+        }
+    })
+})
